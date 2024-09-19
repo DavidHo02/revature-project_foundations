be6799beb46c6bf50de60e157e7f7d5d6c015443
David Ho <davidho6308493@gmail.com>
2024-09-18 21:52:05 -0700

    finished implementation of unit testing for /login

--- __test__/userFunctions.test.js
+++ __test__/userFunctions.test.js
@@ -1,12 +1,14 @@
 const { User } = require('../src/models/user');
 const { registerUser, login } = require('../src/userFunctions');
 
+// partial mocking of userDAO.js to only mock queryUserByUsername function
+// https://jestjs.io/docs/mock-functions#mocking-partials
 jest.mock('../src/userDAO', () => {
     const originalModule = jest.requireActual('../src/userDAO');
 
     return {
         ...originalModule,
-        queryUserByUsername: jest.fn().mockReturnValue({
+        queryUserByUsername: jest.fn().mockReturnValueOnce({
             password: 'david123',
             username: 'david2',
             role: 'Employee',
@@ -47,7 +49,7 @@ describe('User Creation Tests', () => {
 describe('User Function Tests', () => {
     // const queryUserByUsernameResult = queryUserByUsername();
 
-    // successful login
+    // successful login of Employee account
     test('login should return a role of Employee', () => {
         const username = 'david2';
         const password = 'david123';
@@ -58,19 +60,33 @@ describe('User Function Tests', () => {
             });
     });
 
-    // successful login
-    // test('login should return a role of Manager', () => {
-    //     const username = 'admin';
-    //     const password = 'admin';
-    //     return login(username, password)
-    //         .then(data => expect(data).toBe('Manager'));
-    // });
+    // successful login of Manager account
+    test('login should return a role of Manager', () => {
+        const username = 'admin';
+        const password = 'admin';
+        queryUserByUsername.mockReturnValueOnce({
+            password: 'admin',
+            username: 'admin',
+            role: 'Manager',
+            join_date: 1726689060,
+            employee_id: '1ea231df-a30e-424f-be5b-ff8c3c7db266'
+        });
+        return login(username, password)
+            .then((data) => {
+                expect(queryUserByUsername).toHaveBeenCalled();
+                expect(data).toBe('Manager');
+            });
+    });
 
     // failed login
-    // test('login should return false', () => {
-    //     const username = 'dav';
-    //     const password = 'david123';
-    //     login(username, password)
-    //         .then(data => expect(data).toBe(false));
-    // })
+    test('login should return false', () => {
+        const username = 'dav';
+        const password = 'david123';
+        queryUserByUsername.mockReturnValueOnce(false);
+        return login(username, password)
+            .then((data) => {
+                expect(queryUserByUsername).toHaveBeenCalled();
+                expect(data).toBe(false);
+            });
+    })
 });
