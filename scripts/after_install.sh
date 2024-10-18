6ce662658bf804708a0443116e7abe1f3bace937
Ubuntu <ubuntu@ip-172-31-21-155.us-west-1.compute.internal>
2024-10-18 19:12:51 +0000

    trying to configure to work with aws pipeline

---
+++ scripts/after_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/revature-project_foundations-dest
+npm install
---
+++ scripts/application_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/revature-project_foundations-dest
+node index.js > /dev/null 2>&1 &
